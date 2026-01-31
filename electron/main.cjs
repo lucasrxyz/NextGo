f34b3639f1361ba3a2505ef8f7f258ed3c064005
Lucas xryz <lucas.deudon04@gmail.com>
2026-01-31 01:32:09 +0100

    feat : appli exectuable

---
+++ electron/main.cjs
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+  } else {
+    win.loadFile(
+      path.join(__dirname, '../next_go/dist/index.html')
+    )
+  }
+}
+
+app.whenReady().then(createWindow)
